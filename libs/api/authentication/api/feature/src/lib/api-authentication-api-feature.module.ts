8c674bbe994b4d6c77d603a4b67df25fef2981c1
MoeMoola3 <73942516+MoeMoola3@users.noreply.github.com>
2022-04-15 15:07:28 +0200

    update

--- libs/api/authentication/api/feature/src/lib/api-authentication-api-feature.module.ts
+++ libs/api/authentication/api/feature/src/lib/api-authentication-api-feature.module.ts
@@ -1,27 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
-<<<<<<< HEAD
-import { UsersResolver } from './users/users.resolver';
-
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { UsersService } from './users/users.service';
-
-@Module({
-  imports: [
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      autoSchemaFile: 'schema.gql',
-    }),
-    UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
-=======
 // import { UsersResolver } from './users/users.resolver';
 
 @Module({
   imports: [UsersModule],
   controllers: [],
   providers: [],
->>>>>>> 1ef2e38d347f41a3b00163fa6c12f8bf222a9b6e
 })
 export class ApiAuthenticationApiFeatureModule {}
